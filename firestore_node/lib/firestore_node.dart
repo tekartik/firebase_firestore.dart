829ebd9b3ff51622f266a8c88ae12f11d17941af
alex <alex@tekartik.com>
2019-01-28 14:26:42 +0100

    feat: expose firestoreServiceNode

--- firestore_node/lib/firestore_node.dart
+++ firestore_node/lib/firestore_node.dart
@@ -2,4 +2,5 @@ import 'package:tekartik_firebase_firestore/firestore.dart';
 import 'package:tekartik_firebase_firestore_node/src/firestore_node.dart'
     as firestore_node;
 
-FirestoreService get firestoreService => firestore_node.firestoreService;
+FirestoreService get firestoreService => firestoreServiceNode;
+FirestoreService get firestoreServiceNode => firestore_node.firestoreService;
