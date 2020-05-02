ba1ccc1f56ac00e8376ed94e8aff717643fb290c
alex <alex@tekartik.com>
2020-05-02 17:55:53 +0200

    build: firebase_rest 0.6.3

--- firestore_rest/test/firestore_rest_impl_test.dart
+++ firestore_rest/test/firestore_rest_impl_test.dart
@@ -1,3 +1,6 @@
+import 'package:http/http.dart';
+import 'package:tekartik_firebase/firebase.dart';
+import 'package:tekartik_firebase_firestore_rest/firestore_rest.dart';
 import 'package:tekartik_firebase_firestore_rest/src/firestore/v1beta1.dart'
     as api;
 import 'package:tekartik_firebase_firestore_rest/src/import.dart';
@@ -7,6 +10,12 @@ import 'test_setup.dart';
 
 Future main() async {
   final context = await setup();
+  AppOptions accessTokenAppOptions;
+  if (context != null) {
+    accessTokenAppOptions = await getAppOptionsFromAccessToken(
+        Client(), context.accessToken.data,
+        projectId: context.options.projectId, scopes: firebaseBaseScopes);
+  }
   print(context);
   group('rest', () {
     test('basic_googleapis', () async {
@@ -18,6 +27,14 @@ Future main() async {
       var data = await firestoreApi.projects.databases.documents.get(
           'projects/tekartik-free-dev/databases/(default)/documents/tests/data-types');
       print(jsonPretty(data.toJson()));
+    });
+
+    test('access_token', () async {
+      var app = firebaseRest.initializeApp(
+          options: accessTokenAppOptions, name: 'access_token');
+      var firestore = firestoreServiceRest.firestore(app);
+      var snapshot = await firestore.doc('validate_user_access/_dummy').get();
+      expect(snapshot.exists, isFalse);
     });
   }, skip: context == null);
 
