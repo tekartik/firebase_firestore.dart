97f517b7dbadb38c2155f082774aa70f053a5eef
alex <alex@tekartik.com>
2020-08-07 10:55:44 +0200

    fix: travis tests

--- firestore_sim/lib/src/firestore_sim_client.dart
+++ firestore_sim/lib/src/firestore_sim_client.dart
@@ -89,6 +89,7 @@ class DocumentReferenceSim
 
   DocumentReferenceSim(Firestore firestore, String path) {
     init(firestore, path);
+    checkDocumentReferencePath(this.path);
   }
 
   @override
@@ -440,7 +441,9 @@ class CollectionReferenceSim extends Object
   @override
   final String path;
 
-  CollectionReferenceSim(this.firestoreSim, this.path);
+  CollectionReferenceSim(this.firestoreSim, this.path) {
+    checkCollectionReferencePath(path);
+  }
 
   @override
   Future<DocumentReference> add(Map<String, dynamic> data) async {
