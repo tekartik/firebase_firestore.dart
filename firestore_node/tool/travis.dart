9bbdde0254139cdd89c3e5b92aeec1382a766212
alex <alex@tekartik.com>
2020-09-02 18:17:35 +0200

    test: remove node test on travis

--- firestore_node/tool/travis.dart
+++ firestore_node/tool/travis.dart
@@ -1,5 +1,4 @@
 import 'package:process_run/shell.dart';
-import 'package:tekartik_app_node_build/app_build.dart';
 
 Future main() async {
   var shell = Shell();
@@ -9,5 +8,5 @@ dartanalyzer --fatal-warnings --fatal-infos .
 dartfmt -n --set-exit-if-changed .
 pub run test -p vm
 ''');
-  await nodeRunTest();
+  // await nodeRunTest();
 }
