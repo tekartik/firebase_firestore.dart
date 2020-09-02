91af961e2bd6c6aabada496d62b055a7813f71cc
alex <alex@tekartik.com>
2020-09-02 18:05:26 +0200

    test: npm install before

--- firestore_node/tool/travis.dart
+++ firestore_node/tool/travis.dart
@@ -1,4 +1,5 @@
 import 'package:process_run/shell.dart';
+import 'package:tekartik_app_node_build/app_build.dart';
 
 Future main() async {
   var shell = Shell();
@@ -6,6 +7,7 @@ Future main() async {
   await shell.run('''
 dartanalyzer --fatal-warnings --fatal-infos .
 dartfmt -n --set-exit-if-changed .
-pub run test -p vm,node
+pub run test -p vm
 ''');
+  await nodeRunTest();
 }
