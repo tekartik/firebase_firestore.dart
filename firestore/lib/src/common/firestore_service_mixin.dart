83baecc9c4bfa9c24a68d62baeafb7f8913d63c7
alex <alex@tekartik.com>
2020-09-25 11:24:22 +0200

    feat: allow creating new in memory instance

--- firestore/lib/src/common/firestore_service_mixin.dart
+++ firestore/lib/src/common/firestore_service_mixin.dart
@@ -3,7 +3,7 @@ import 'package:tekartik_firebase_firestore/firestore.dart';
 
 mixin FirestoreServiceMixin implements FirestoreService {
   /// Most implementation need a single instance, keep it in memory!
-  static final _instances = <App, Firestore>{};
+  final _instances = <App, Firestore>{};
 
   T getInstance<T extends Firestore>(App app, T Function() createIfNotFound) {
     var instance = _instances[app] as T;
