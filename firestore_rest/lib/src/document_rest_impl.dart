0691e854a85771a9ac3a93f1945d770a2cb81d3a
alex <alex@tekartik.com>
2021-04-05 11:36:22 +0200

    rest: fix toRest value

--- firestore_rest/lib/src/document_rest_impl.dart
+++ firestore_rest/lib/src/document_rest_impl.dart
@@ -1,6 +1,7 @@
 import 'package:tekartik_firebase_firestore/firestore.dart';
 import 'package:tekartik_firebase_firestore_rest/src/firestore/v1_fixed.dart';
 import 'package:tekartik_firebase_firestore_rest/src/firestore_rest_impl.dart';
+import 'firestore_rest_impl.dart' as rest;
 
 abstract class FirestoreDocumentContext {
   FirestoreRestImpl get impl;
@@ -15,73 +16,6 @@ mixin DocumentContext {
   FirestoreDocumentContext get firestore;
 
   Value toRestValue(dynamic value) {
-    Value restValue;
-
-    if (value == null) {
-      restValue = Value()..nullValue = 'NULL_VALUE';
-    } else if (value is String) {
-      restValue = Value()..stringValue = value;
-    } else if (value is bool) {
-      restValue = Value()..booleanValue = value;
-    } else if (value is int) {
-      restValue = Value()..integerValue = value.toString();
-    } else if (value is double) {
-      restValue = Value()..doubleValue = value;
-    } else if (value is GeoPoint) {
-      var geoPointValue = LatLng()
-        ..latitude = value.latitude.toDouble()
-        ..longitude = value.longitude.toDouble();
-      restValue = Value()..geoPointValue = geoPointValue;
-    } else if (value is Timestamp) {
-      restValue = Value()..timestampValue = value.toString();
-    } else if (value is DateTime) {
-      restValue = Value()
-        ..timestampValue = Timestamp.fromDateTime(value).toIso8601String();
-    } else if (value is Map) {
-      restValue = _mapToRestValue(value);
-    } else if (value is Iterable) {
-      restValue = _listToRestValue(value);
-    } else if (value is Blob) {
-      restValue = Value()..bytesValueAsBytes = value.data;
-    } else if (value is DocumentReference) {
-      restValue = Value()
-        ..referenceValue = firestore.getDocumentName(value.path);
-      // else  if (value is FieldValue) {
-      //restValue = Value()..nullValue= 'NULL_VALUE';
-    } else if (value is FieldValue) {
-      if (value == FieldValue.serverTimestamp) {
-        // TODO for now use local date time
-        restValue = Value()..timestampValue = Timestamp.now().toIso8601String();
-      } else {
-        throw UnsupportedError('type ${value.runtimeType}: $value');
-      }
-
-      //      throw UnsupportedError('type ${value.runtimeType}: $value');
-    } else {
-      throw UnsupportedError('type ${value.runtimeType}: $value');
-    }
-    return restValue;
-  }
-
-  Value _mapToRestValue(Map map) {
-    var mapValue = MapValue()..fields = _mapToFields(map);
-    return Value()..mapValue = mapValue;
-  }
-
-  Map<String, Value> _mapToFields(Map map) {
-    var fields =
-        map.map((key, value) => MapEntry(key.toString(), toRestValue(value)));
-    return fields;
-  }
-
-  void fromMap(Map map) {
-    map.map((key, value) => MapEntry(key?.toString(), toRestValue(value)));
-  }
-
-  Value _listToRestValue(Iterable list) {
-    var arrayValue = ArrayValue()
-      ..values =
-          list.map((value) => toRestValue(value)).toList(growable: false);
-    return Value()..arrayValue = arrayValue;
+    return rest.toRestValue(firestore.impl, value);
   }
 }
