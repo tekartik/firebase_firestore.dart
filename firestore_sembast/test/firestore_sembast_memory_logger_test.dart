f725b4acd8b6fe5911b9a79328cd6713fc119474
alex <alex@tekartik.com>
2021-10-11 09:21:59 +0200

    fix: dev lints

--- firestore_sembast/test/firestore_sembast_memory_logger_test.dart
+++ firestore_sembast/test/firestore_sembast_memory_logger_test.dart
@@ -4,11 +4,32 @@ import 'package:tekartik_firebase_firestore/firestore_logger.dart';
 import 'package:tekartik_firebase_firestore_sembast/firestore_sembast.dart';
 import 'package:tekartik_firebase_firestore_test/firestore_test.dart';
 import 'package:tekartik_firebase_local/firebase_local.dart';
+import 'package:test/test.dart';
 
 void main() {
   // needed for memory
   skipConcurrentTransactionTests = true;
   var firebase = FirebaseLocal();
+
+  // Basic check for set
+  test('set', () async {
+    var firestore = newFirestoreMemory();
+    var events = <FirestoreLoggerEvent>[];
+    firestore = FirestoreLogger(
+        firestore: firestore,
+        options: FirestoreLoggerOptions.all(log: (event) {
+          events.add(event);
+        }));
+    var ref = firestore.doc('test/1');
+    await ref.set({});
+    expect(events.length, 1);
+
+    events.clear();
+    await firestore.runTransaction((transaction) {
+      transaction.set(ref, {});
+    });
+    expect(events.length, 1);
+  });
   run(
       firebase: firebase,
       firestoreService: FirestoreServiceLogger(
