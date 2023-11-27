5e79fa678dd06aea426853c980f32355a994e3fa
Alexandre Roux <alex@tekartik.com>
2023-11-27 15:29:19 +0100

    fix: tests

--- repo_support/tool/quick_test_idb_sembast_memory.dart
+++ repo_support/tool/quick_test_idb_sembast_memory.dart
@@ -7,7 +7,7 @@ Future<void> main() async {
   var shell = Shell(workingDirectory: join(topDir, 'firestore_idb'));
   await shell.run('dart test -p vm test/firestore_idb_test.dart');
   shell = Shell(workingDirectory: join(topDir, 'firestore_sembast'));
-  await shell.run('dart test -p vm test/firestore_sembast_memory_test.dart');
+  await shell.run('dart test -p vm test/firestore_sembast_test.dart');
   shell = Shell(workingDirectory: join(topDir, 'firestore'));
   await shell.run('dart test -p vm');
 }
