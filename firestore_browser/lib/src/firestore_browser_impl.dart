f4ada4616dd618bfad9362a2b7595071bb3f26e6
Alexandre Roux <alex@tekartik.com>
2024-09-15 18:12:53 +0200

    fix lints

--- firestore_browser/lib/src/firestore_browser_impl.dart
+++ firestore_browser/lib/src/firestore_browser_impl.dart
@@ -1,5 +1,6 @@
 import 'package:js/js_util.dart';
-import 'package:tekartik_browser_utils/browser_utils_import.dart';
+import 'package:tekartik_browser_utils/js_loader_utils.dart';
+import 'package:tekartik_common_utils/common_utils_import.dart';
 import 'package:tekartik_firebase/firebase_mixin.dart';
 
 import 'import_browser.dart';
