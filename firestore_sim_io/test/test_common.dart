3134437713759b6c48e1b216ffa001ba7910b774
Alexandre Roux <alex@tekartik.com>
2024-09-26 11:41:54 +0200

    fix lints

--- firestore_sim_io/test/test_common.dart
+++ firestore_sim_io/test/test_common.dart
@@ -5,7 +5,6 @@ import 'package:tekartik_firebase_firestore_sim/src/firestore_sim_server.dart';
 import 'package:tekartik_firebase_local/firebase_local.dart';
 import 'package:tekartik_firebase_sim/firebase_sim_server.dart';
 import 'package:tekartik_firebase_sim_io/firebase_sim_client_io.dart' as sim;
-import 'package:tekartik_web_socket/web_socket.dart';
 import 'package:tekartik_web_socket_io/web_socket_io.dart';
 
 class TestContext {
