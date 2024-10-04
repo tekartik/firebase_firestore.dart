9e87c9369a6b36bf1bd401075aadebfd4143ff90
Alexandre Roux <alex@tekartik.com>
2024-10-04 21:27:37 +0200

    re-export old json utils helpers

--- firestore/lib/utils/json_utils.dart
+++ firestore/lib/utils/json_utils.dart
@@ -1,4 +1,10 @@
 export 'package:tekartik_firebase_firestore/src/firestore_common.dart'
-    show documentDataMapToJsonMap, snapshotDataToJsonMap;
+    show
+        documentDataMapToJsonMap,
+        snapshotDataToJsonMap,
+        documentDataValueToJson,
+        documentDataMapFromJsonMap,
+        documentDataFromJsonMap,
+        documentDataFromJsonMapNoFirestore;
 export 'package:tekartik_firebase_firestore/src/snapshot_json.dart'
     show FirestoreDocumentInfo;
